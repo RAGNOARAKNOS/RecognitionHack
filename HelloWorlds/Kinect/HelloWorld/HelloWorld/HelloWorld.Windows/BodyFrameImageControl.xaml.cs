4ce04e51bda27400f2590bc7c95d148aa233eb6c
RAGNOARAKNOS <ragnocorp@googlemail.com>
2016-06-25 19:20:28 +0100

    Basic Debug

--- HelloWorlds/Kinect/HelloWorld/HelloWorld/HelloWorld.Windows/BodyFrameImageControl.xaml.cs
+++ HelloWorlds/Kinect/HelloWorld/HelloWorld/HelloWorld.Windows/BodyFrameImageControl.xaml.cs
@@ -1,92 +1,97 @@
-﻿namespace HelloWorld
+﻿using System.Diagnostics;
+
+namespace HelloWorld
 {
-  using Windows.Foundation;
-  using Windows.UI;
-  using Windows.UI.Xaml.Controls;
-  using Windows.UI.Xaml.Media;
-  using Windows.UI.Xaml.Shapes;
-  using WindowsPreview.Kinect;
+    using Windows.Foundation;
+    using Windows.UI;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Media;
+    using Windows.UI.Xaml.Shapes;
+    using WindowsPreview.Kinect;
 
-  public partial class BodyFrameImageControl : UserControl
-  {
-    public BodyFrameImageControl()
+    public partial class BodyFrameImageControl : UserControl
     {
-      InitializeComponent();
-    }
-    public void Initialise(FrameDescription colorFrameDescription,
-      CoordinateMapper coordinateMapper)
-    {
-      this.colorFrameDescription = colorFrameDescription;
-      this.coordinateMapper = coordinateMapper;
-    }
-    public void DrawBodies(Body[] bodies)
-    {
-      // We take the naive approach of getting rid of everything for now in the
-      // hope of simplicity. We could do something better. 
-      this.canvas.Children.Clear();
-
-      for (int i = 0; i < bodies.Length; i++)
-      {
-        if (bodies[i].IsTracked)
+        public BodyFrameImageControl()
         {
-          this.DrawBody(bodies[i], BodyBrushes[i]);
+            InitializeComponent();
         }
-      }
-    }
-    void DrawBody(Body body, Brush brush)
-    {
-      foreach (var entry in body.Joints)
-      {
-        JointType jointType = entry.Key;
-        Joint joint = entry.Value;
-
-        if (joint.TrackingState != TrackingState.NotTracked)
+        public void Initialise(FrameDescription colorFrameDescription,
+          CoordinateMapper coordinateMapper)
         {
-          Point position2d = this.MapPointToCanvasSpace(joint.Position);
-
-          if (!double.IsInfinity(position2d.X) && !double.IsInfinity(position2d.Y))
-          {
-            Ellipse ellipse =
-              this.MakeEllipseForJoint(jointType, joint.TrackingState, brush, position2d);
-
-            this.canvas.Children.Add(ellipse);
-          }
+            this.colorFrameDescription = colorFrameDescription;
+            this.coordinateMapper = coordinateMapper;
         }
-      }
-    }
-    Ellipse MakeEllipseForJoint(
-      JointType jointType, 
-      TrackingState trackingState, 
-      Brush brush, 
-      Point position2d)
-    {
-      int width = jointType == JointType.Head ? HEAD_WIDTH : REGULAR_WIDTH;
+        public void DrawBodies(Body[] bodies)
+        {
+            // We take the naive approach of getting rid of everything for now in the
+            // hope of simplicity. We could do something better. 
+            this.canvas.Children.Clear();
 
-      Ellipse ellipse = new Ellipse()
-      {
-        Width = width,
-        Height = width,
-        Fill = trackingState == TrackingState.Inferred ? InferredBrush : brush
-      };
-      Canvas.SetLeft(ellipse, position2d.X - (width / 2));
-      Canvas.SetTop(ellipse, position2d.Y - (width / 2));
-      return (ellipse);
-    }
-    Point MapPointToCanvasSpace(CameraSpacePoint point)
-    {
-      ColorSpacePoint colorSpacePoint = 
-        this.coordinateMapper.MapCameraPointToColorSpace(point);
+            for (int i = 0; i < bodies.Length; i++)
+            {
+                if (bodies[i].IsTracked)
+                {
+                    this.DrawBody(bodies[i], BodyBrushes[i]);
+                }
+            }
+        }
+        void DrawBody(Body body, Brush brush)
+        {
 
-      Point mappedPoint = new Point(
-        colorSpacePoint.X / this.colorFrameDescription.Width * this.canvas.ActualWidth,
-        colorSpacePoint.Y / this.colorFrameDescription.Height * this.canvas.ActualHeight);
+            Debug.WriteLine("Body ID: {0} : {1}", body.ToString(), body.TrackingId);
 
-      return (mappedPoint);
-    }  
-    static readonly int HEAD_WIDTH = 50;
-    static readonly int REGULAR_WIDTH = 20;
-    static readonly Brush[] BodyBrushes = 
-    {
+            foreach (var entry in body.Joints)
+            {
+                JointType jointType = entry.Key;
+                Joint joint = entry.Value;
+
+                if (joint.TrackingState != TrackingState.NotTracked)
+                {
+                    Point position2d = this.MapPointToCanvasSpace(joint.Position);
+
+                    if (!double.IsInfinity(position2d.X) && !double.IsInfinity(position2d.Y))
+                    {
+                        Ellipse ellipse =
+                          this.MakeEllipseForJoint(jointType, joint.TrackingState, brush, position2d);
+
+                        this.canvas.Children.Add(ellipse);
+                    }
+                }
+            }
+        }
+        Ellipse MakeEllipseForJoint(
+          JointType jointType,
+          TrackingState trackingState,
+          Brush brush,
+          Point position2d)
+        {
+            int width = jointType == JointType.Head ? HEAD_WIDTH : REGULAR_WIDTH;
+
+            Ellipse ellipse = new Ellipse()
+            {
+                Width = width,
+                Height = width,
+                Fill = trackingState == TrackingState.Inferred ? InferredBrush : brush
+            };
+            Canvas.SetLeft(ellipse, position2d.X - (width / 2));
+            Canvas.SetTop(ellipse, position2d.Y - (width / 2));
+            return (ellipse);
+        }
+        Point MapPointToCanvasSpace(CameraSpacePoint point)
+        {
+            ColorSpacePoint colorSpacePoint =
+              this.coordinateMapper.MapCameraPointToColorSpace(point);
+
+            Point mappedPoint = new Point(
+              colorSpacePoint.X / this.colorFrameDescription.Width * this.canvas.ActualWidth,
+              colorSpacePoint.Y / this.colorFrameDescription.Height * this.canvas.ActualHeight);
+
+            return (mappedPoint);
+        }
+        static readonly int HEAD_WIDTH = 50;
+        static readonly int REGULAR_WIDTH = 20;
+        static readonly Brush[] BodyBrushes =
+        {
       new SolidColorBrush(Colors.Red),
       new SolidColorBrush(Colors.Green),
       new SolidColorBrush(Colors.Blue),
@@ -94,10 +99,12 @@
       new SolidColorBrush(Colors.Black),
       new SolidColorBrush(Colors.Orange)
     };
-    static readonly Brush InferredBrush = new SolidColorBrush(Colors.Gray);
-    FrameDescription colorFrameDescription;
-    CoordinateMapper coordinateMapper;
+        static readonly Brush InferredBrush = new SolidColorBrush(Colors.Gray);
+        FrameDescription colorFrameDescription;
+        CoordinateMapper coordinateMapper;
 
-    public static object Brushes { get; private set; }
-  }
+        private static JointType[] renderedJointTypes = {JointType.Head, JointType.HandLeft, JointType.HandRight};
+
+        public static object Brushes { get; private set; }
+    }
 }
